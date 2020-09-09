33a9cf8465676aad0ede54bb81b0847823852dd4
s-t-a-n <stan@wopwop.cc>
2020-09-09 02:31:05 +0200

    changed varname n -> is_malloced for clarity 2

--- src/ops/destroy.c
+++ src/ops/destroy.c
@@ -23,8 +23,8 @@ void	*vec_destroy(void **root, size_t is_malloced, void *obj)
 	{
 		while(vec->size > 0)
 		{
-			free(vec_peekback(root, n, obj));
-			vec_popback(root, n, obj);
+			free(vec_peekback(root, 0, obj));
+			vec_popback(root, 0, obj);
 		}
 	}
 	free(vec->mem);

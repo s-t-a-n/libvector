afd2d18fefc80f39437a05602ece1b00d7733fc1
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2021-02-16 23:04:59 +0100

    Overhaul: Cmake and Catch2 implemented (#2)
    * Overhaul: Cmake and Catch2 implemented
    
    * fixed ISO C++ standard errors in basics test

---
+++ README.md
@@ -0,0 +1,5 @@
+# Libvector
+## A vector library for C.
+[![build](https://github.com/s-t-a-n/libvector/workflows/Unittests/badge.svg)](https://github.com/s-t-a-n/libvector/actions?workflow=Unittests)
+[![linux](https://github.com/s-t-a-n/libvector/workflows/Linux/badge.svg)](https://github.com/s-t-a-n/libvector/actions?workflow=Linux)
+[![macos](https://github.com/s-t-a-n/libvector/workflows/MacOS/badge.svg)](https://github.com/s-t-a-n/libvector/actions?workflow=MacOS)
---
+++ tests/src/basics.cpp
@@ -0,0 +1,60 @@
+#include <catch2/catch.hpp>
+
+extern "C" {
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include "vector.h"
+}
+
+TEST_CASE( "create_destroy", "[vector]" ) {
+	void *root;
+
+	size_t count = 8;
+	CHECK(vector(&root, V_CREATE, count, NULL));
+
+	CHECK(vector(&root, V_DESTROY, 0, NULL) == NULL);
+}
+
+TEST_CASE( "push_pop", "[vector]" ) {
+	void	*root;
+	char	*str = (char *)"Vector life.";
+	size_t	size = 1;
+
+	CHECK(vector(&root, V_CREATE, size, NULL));
+	CHECK(vector(&root, V_PUSHBACK, 0, str));
+	printf("%s\n", (char *)vector(&root, V_PEEKBACK, 0, NULL));
+	CHECK(memcmp(vector(&root, V_PEEKBACK, 0, NULL), str, strlen(str)) == 0);
+	CHECK(vector(&root, V_POPBACK, 0, NULL) == NULL);
+	CHECK(*(size_t *)vector(&root, V_SIZE, 0, NULL) == 0);
+
+	CHECK(vector(&root, V_DESTROY, 0, NULL) == NULL);
+}
+
+TEST_CASE( "push_realloc", "[vector]" ) {
+	void	*root;
+	size_t	size = 1;
+	size_t	run_size = 50;
+
+	CHECK(vector(&root, V_CREATE, size, NULL));
+	for (unsigned int i = 0; i < run_size; i++)
+	{
+		char *str = (char *)calloc(1024, 1);
+		CHECK(vector(&root, V_PUSHBACK, 0, str));
+	}
+
+	char *cmpstr = (char *)calloc(1024, 1);
+	for (unsigned int i = 0; i < run_size; i++)
+	{
+		char *str = (char *)vector(&root, V_PEEKBACK, 0, NULL);
+		CHECK(str);
+		CHECK(memcmp(str, cmpstr, 1024) == 0);
+		free(str);
+		CHECK(vector(&root, V_POPBACK, 0, NULL) == NULL);
+	}
+	free(cmpstr);
+	CHECK(*(size_t *)vector(&root, V_SIZE, 0, NULL) == 0);
+	CHECK(vector(&root, V_DESTROY, 0, NULL) == NULL);
+}

5c4087aa274ed4944198eacc0739ba75b7c1be0e
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-15 23:50:02 +0100

    src/generic resize.c -> reallocate.c

--- src/generic/reallocate.c
+++ src/generic/reallocate.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   resize.c                                           :+:    :+:            */
+/*   reallocate.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: sverschu <sverschu@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/07 14:49:11 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/08/07 16:22:20 by sverschu      ########   odam.nl         */
+/*   Updated: 2021/03/15 16:22:20 by sverschu      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector_internal.h"
 
-void	*resize(t_vector *vec, size_t cap, size_t new_cap)
+void	*reallocate(t_vector *vec, size_t cap, size_t new_cap)
 {
 	void	*nmem;
 
